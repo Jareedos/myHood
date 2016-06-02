40b5fa0b269eef90cab1b1719ee70d177a6d562f
Jareedos <jaredallens@gmail.com>
2016-06-02 16:17:07 -0700

    added View folder and postcell file

---
+++ myHood/PostCell.swift
@@ -0,0 +1,18 @@
+//
+//  PostCell.swift
+//  myHood
+//
+//  Created by Jared Sobol on 6/2/16.
+//  Copyright © 2016 Appmaker. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
