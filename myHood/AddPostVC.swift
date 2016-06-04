a87ff1d95f9e3df3424da4852df3ac95d589ddc7
Jareedos <jaredallens@gmail.com>
2016-06-04 10:40:49 -0700

    added imagePicker & delegate to PostCellVC

--- myHood/AddPostVC.swift
+++ myHood/AddPostVC.swift
@@ -8,20 +8,25 @@
 
 import UIKit
 
-class AddPostVC: UIViewController {
+class AddPostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var postImg: UIImageView!
-
     @IBOutlet weak var titleField: UITextField!
-    
     @IBOutlet weak var descField: UITextField!
+    
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         postImg.layer.cornerRadius = postImg.frame.size.width / 2
         postImg.clipsToBounds = true
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
     }
     
     @IBAction func makePostBtnPressed(sender: AnyObject) {
+        if let title = titleField.text, let desc = descField.text, let img = postImg.image {
+            
+        }
         
     }
     
@@ -32,5 +37,11 @@ class AddPostVC: UIViewController {
     
     @IBAction func addPicBtnPressed(sender: UIButton!) {
         sender.setTitle("", forState: .Normal )
+        presentViewController(<#T##viewControllerToPresent: UIViewController##UIViewController#>, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        postImg.image = image
     }
 }
