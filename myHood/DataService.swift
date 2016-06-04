0df0c983317389fa2ecb7c607cf865fc26c49aa4
Jareedos <jaredallens@gmail.com>
2016-06-04 10:48:21 -0700

    added Services Folder & DataService file with my DataService Singlton in it

---
+++ myHood/DataService.swift
@@ -0,0 +1,13 @@
+//
+//  DataService.swift
+//  myHood
+//
+//  Created by Jared Sobol on 6/4/16.
+//  Copyright © 2016 Appmaker. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+}
