7ec09c804ee868602d618bf8de0502855c10de17
Jareedos <jaredallens@gmail.com>
2016-06-03 21:17:44 -0700

    added controller folder & AddPostVC file and VC

---
+++ myHood/AddPostVC.swift
@@ -0,0 +1,18 @@
+//
+//  AddPostVC.swift
+//  myHood
+//
+//  Created by Jared Sobol on 6/3/16.
+//  Copyright © 2016 Appmaker. All rights reserved.
+//
+
+import UIKit
+
+class AddPostVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
