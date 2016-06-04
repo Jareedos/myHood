0c578cda5258048a93c8844de570f526f87c0d6e
Jareedos <jaredallens@gmail.com>
2016-06-04 10:23:40 -0700

    added Actions and Outlets and made the image a circle

--- myHood/AddPostVC.swift
+++ myHood/AddPostVC.swift
@@ -9,10 +9,28 @@
 import UIKit
 
 class AddPostVC: UIViewController {
+    @IBOutlet weak var postImg: UIImageView!
 
+    @IBOutlet weak var titleField: UITextField!
+    
+    @IBOutlet weak var descField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        postImg.layer.cornerRadius = postImg.frame.size.width / 2
+        postImg.clipsToBounds = true
+    }
+    
+    @IBAction func makePostBtnPressed(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func cancelBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
+    
+    @IBAction func addPicBtnPressed(sender: UIButton!) {
+        sender.setTitle("", forState: .Normal )
     }
 }
