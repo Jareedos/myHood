4fec6abceea5bcc5eac4fecc1404e813e9d0a1fa
Jareedos <jaredallens@gmail.com>
2016-06-08 15:11:34 -0700

    added dismiss functionality

--- myHood/AddPostVC.swift
+++ myHood/AddPostVC.swift
@@ -27,6 +27,7 @@ class AddPostVC: UIViewController, UIImagePickerControllerDelegate, UINavigation
         if let title = titleField.text, let desc = descField.text, let img = postImg.image {
             let post = Post(imagePath: "", title: title, descriptin: desc)
             DataService.instance.addPost(post)
+            dismissViewControllerAnimated(true, completion: nil)
         }
         
     }
